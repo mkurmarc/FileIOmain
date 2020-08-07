9494cb3127bef7220b43dfb5c6f0c951306e989a
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2020-08-07 08:11:05 -0700

    Added code to: create & open file, get items & write to file, close file;

--- src/FileIOmain/FileIOmain.java
+++ src/FileIOmain/FileIOmain.java
@@ -1,21 +1,38 @@
 package FileIOmain;
 
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FileIOmain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         String filename = "groceries.txt", item;
 
          // file name and item variables
         Scanner keyboard = new Scanner(System.in);
 
-        // Get item count
+        // get item count
         System.out.print("How many items do you have? ");
-        int numItems = keyboard.nextInt();
+        int numItems = keyboard.nextInt(); // using nextInt() leaves data inside of memory, keyboard buffer, erase it
 
         // clear keyboard buffer
         keyboard.nextLine();
+
+        //create and open file
+        PrintWriter outputFile = new PrintWriter(filename);
+
+        // get items and write to file
+        for(int i = 0; i < numItems; i++) {
+            System.out.print("Enter item " + (i + 1) + ": ");
+            item = keyboard.nextLine();
+            outputFile.println(item);
+        }
+
+        // close file
+        outputFile.close();
+        System.out.println("File written.");
 
     }
 }
